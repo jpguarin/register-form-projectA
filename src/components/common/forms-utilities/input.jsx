17e135153feabd84d3c948d4b181224f58e25d78
Juan Pablo Guarin <jpguarin@gmail.com>
2019-09-01 23:48:57 -0500

    component –› Form Utilities Input add error behavior

--- src/components/common/forms-utilities/input.jsx
+++ src/components/common/forms-utilities/input.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./form-utilities.scss";
 
-const Input = ({ name, label, ...rest }) => {
+const Input = ({ name, label, error, ...rest }) => {
+  const hasErrorstyle = error ? ' has-error ' : '';
   return (
-    <div className="field-wrapper">
+    <div className={"field-wrapper" + hasErrorstyle}>
       { label && (
         <label htmlFor={name}>{label}</label>
       )}
       <input {...rest} name={name} id={name} />
+      { !(name === 'password') &&
+      <p className="error">{error}</p>
+      }
     </div>
   );
 };
